75d8304489123c166d5ba27f6d0c4b9e4ceaf17b
caitlingraves <100045372+caitlingraves@users.noreply.github.com>
2024-04-03 09:36:53 -0700

    working login

--- routes/index.js
+++ routes/index.js
@@ -21,45 +21,51 @@ router.get('/calendar', function(req, res, next) {
   if(req.query.msg){
     res.locals.msg = req.query.msg
   }
-  res.render('login');
+  res.render('calendar');
 });
 
 router.get('/aboutus', function(req, res, next) {
   if(req.query.msg){
     res.locals.msg = req.query.msg
   }
-  res.render('login');
+  res.render('aboutus');
 });
 
 router.get('/recipe-search', function(req, res, next) {
   if(req.query.msg){
     res.locals.msg = req.query.msg
   }
-  res.render('login');
+  res.render('recipe-search');
 });
 
 router.get('/recipe', function(req, res, next) {
   if(req.query.msg){
     res.locals.msg = req.query.msg
   }
-  res.render('login');
+  res.render('recipe');
 });
 
 router.get('/view-user', function(req, res, next) {
   if(req.query.msg){
     res.locals.msg = req.query.msg
   }
-  res.render('login');
+  res.render('view-user');
 });
 
 router.post('/login', async function(req, res, next) {
   //console.log(req.body.username+" - "+req.body.password);
+  console.log("login post")
+  console.log(req.body.username)
   const user = await User.findUser(req.body.username, req.body.password)
   if(user!== null){
     req.session.user = user
-    res.redirect("/courses")
+    res.redirect("/")
+    console.log("user found")
   }else{
-    res.redirect("/?msg=fail")
+    console.log("user not found")
+   // res.redirect("/login");
+    res.locals.msg = "fail";
+    res.render('login');
   }
 });
 
