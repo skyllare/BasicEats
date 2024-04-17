d31fcb632c0cc2e8ff3c5eecd4c601b8c4fea271
mollyiverson <molly_iverson@outlook.com>
2024-04-17 00:08:39 -0700

    Created header template for account details

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const sequelize = require('./db')
 const User = require('./models/User')
 const MealPlan = require('./models/MealPlan')
-
+const Recipe = require('./models/Recipe');
 
 var indexRouter = require('./routes/index');
 var calendarRouter = require('./routes/calendar');
@@ -48,31 +48,31 @@ app.use('/change_password', accountRouter);
 app.use('/calendar', calendarRouter);
 
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 async function setup() {
-  const subu = await User.create({ username: "subu", password: "1234" });
-const meals = await MealPlan.bulkCreate([
-            { username: 'caitlin', day: '2024-04-11', weekday:'Sunday', mealNum: 1, name: "eggs" },
-            { username: 'd', day: '2024-04-12', weekday:'Monday', mealNum: 1, name: "friday" }
-]);
+  const subu = await User.create({ username: "subu", password: "1234", admin: true, recipe_count: 0 });
+  const meals = await MealPlan.bulkCreate([
+    { username: 'caitlin', day: '2024-04-11', weekday: 'Sunday', mealNum: 1, name: "eggs" },
+    { username: 'd', day: '2024-04-12', weekday: 'Monday', mealNum: 1, name: "friday" }
+  ]);
+
+  const saladRecipe = await Recipe.create({ recipeid: "subu1", recipename: "salad", ingredients: "lettuce, dressing, croutons, strawberries, candied pecans", recipedesc: "strawberry salad perfect for summer", instructions: "1. Candy pecans \n 2. Clean strawberries and chop them \n 3. Chop lettuce \n 4. Put ingredients in a bowl and toss \n 5. Add dressing", time: 20 });
 
   console.log("subu instance created...")
-  console.l
 }
 
-sequelize.sync({ force: true }).then(()=>{
+sequelize.sync({ force: true }).then(() => {
   console.log("Sequelize Sync Completed...");
-  setup().then(()=> console.log("User setup complete"))
+  setup().then(() => console.log("User setup complete"))
 })
 
 module.exports = app;
