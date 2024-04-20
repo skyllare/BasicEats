c44c59290291f502386043303b61112b54574b60
mollyiverson <molly_iverson@outlook.com>
2024-04-19 17:54:59 -0700

    Made recipe page for individual user created recipe

--- public/javascript/myrecipe.js
+++ public/javascript/myrecipe.js
@@ -13,10 +13,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const caretPosition = this.selectionStart;
             const lineNumber = text.substr(0, caretPosition).split('\n').length;
-            const newText = text.substring(0, caretPosition) + `\n${lineNumber + 1}. ` + text.substring(caretPosition);
+            const newText = text.substring(0, caretPosition) + `\n${lineNumber + 1}.  ` + text.substring(caretPosition);
             this.value = newText;
 
             this.setSelectionRange(caretPosition + 4, caretPosition + 4);
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const instructionsTextarea = document.getElementById('ingredients');
+
+    instructionsTextarea.value = '- ';
+
+    instructionsTextarea.addEventListener('keydown', function (event) {
+        const text = this.value;
+
+        if (event.key === 'Enter') {
+            event.preventDefault();
+
+            const caretPosition = this.selectionStart;
+
+            const newText = text.substring(0, caretPosition) + '\n-  ' + text.substring(caretPosition);
+            this.value = newText;
+
+            this.setSelectionRange(caretPosition + 3, caretPosition + 3);
         }
     });
 });
